01cb200fd54b53c8fb745c6549bfe2f83344a40e
KasperMin <kaspermsimonsen@gmail.comgit config --global user.email git config --global user.email kaspermsimonsen@gmail.com>
2017-10-17 18:48:29 +0200

    added newSketch function and improved prompt

--- main.js
+++ main.js
@@ -1,39 +1,7 @@
 
 $(document).ready(function() {
 var squaresPrSide = 10;
-var newSketchOn = false;
 createSketch(squaresPrSide);
-
-  function createSketch(squaresPrSide) {
-
-    var colums = squaresPrSide;
-    var rows = squaresPrSide - 1;
-    var width = parseInt(400/squaresPrSide);
-    var height = parseInt(400/squaresPrSide);
-    console.log(width);
-
-    for (var i = 0; i < colums; i++) {
-      $("<div></div>").appendTo(".container").addClass("boxColum").width(width).height(height);
-      for (var j = 0; j < rows; j++) {
-        $("<div></div>").appendTo(".container").addClass("boxRow").width(width).height(height);
-      }
-    }
-    $("div").show();
-  }
-
-  $("button.clear").click(function() {
-    $("div").hide();
-    squaresPrSide = prompt("Enter squares pr. side");
-    squaresPrSide = parseInt(squaresPrSide);
-    if (isNaN(squaresPrSide)) {
-      squaresPrSide = prompt("Invalid input! Please enter a number"); // don't show if cancel
-    }
-    createSketch(squaresPrSide);
-  });
-
-
-
-
 
   function createColor() {
     var letter = "0123456789abcdef";
@@ -44,13 +12,38 @@ createSketch(squaresPrSide);
     return color;
   };
 
-  $("div").on("mouseenter", function() {
-    $(this).css( "background-color", createColor() );
+  function createSketch(squaresPrSide) {
+
+    var colums = squaresPrSide;
+    var rows = squaresPrSide - 1;
+    var width = parseInt(400/squaresPrSide);
+    var height = parseInt(400/squaresPrSide);
+
+    for (var i = 0; i < colums; i++) {
+      $("<div></div>").appendTo(".container").addClass("boxColum").width(width).height(height);
+      for (var j = 0; j < rows; j++) {
+        $("<div></div>").appendTo(".container").addClass("boxRow").width(width).height(height);
+      }
+    }
+
+    $("div").on("mouseenter", function() {
+      $(this).css( "background-color", createColor() );
+    });
+
+  }
+
+  $("button.clear").click(function() {
+    squaresPrSide = prompt("Enter squares pr. side");
+    if (typeof squaresPrSide == "string") {
+      squaresPrSide = parseInt(squaresPrSide);
+    }
+
+    if ( squaresPrSide !== null && isNaN(squaresPrSide) )  {
+      alert("Invalid input! Please enter a number");
+    } else if (typeof squaresPrSide == "number") {
+      $("div").remove(".boxColum").remove(".boxRow");
+      createSketch(squaresPrSide);
+    }
   });
 
 });
-
-
-/*
-
-*/
